2bdf2d84b0687c8112dce2339b76cfc99b2c78de
Usshel <pawelopadczuk@gmail.com>
2023-02-09 21:10:24 +0100

    feat: control-email done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormControlNameComponent } from './components/form-control-name/form-control-name.component';
 import { FormControlAgeComponent } from './components/form-control-age/form-control-age.component';
+import { FormControlEmailComponent } from './components/form-control-email/form-control-email.component';
 import { FormControlNameComponentModule } from './components/form-control-name/form-control-name.component-module';
 import { FormControlAgeComponentModule } from './components/form-control-age/form-control-age.component-module';
+import { FormControlEmailComponentModule } from './components/form-control-email/form-control-email.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'control-name', component: FormControlNameComponent }, { path: 'control-age', component: FormControlAgeComponent }]), FormControlNameComponentModule, FormControlAgeComponentModule],
+  imports: [RouterModule.forRoot([{ path: 'control-name', component: FormControlNameComponent }, { path: 'control-age', component: FormControlAgeComponent }, { path: 'control-email', component: FormControlEmailComponent }]), FormControlNameComponentModule, FormControlAgeComponentModule, FormControlEmailComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/form-control-email/form-control-email.component.ts
+++ src/app/components/form-control-email/form-control-email.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-control-email',
@@ -7,4 +9,9 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormControlEmailComponent {
+  readonly form: FormGroup = new FormGroup({ email: new FormControl('', Validators.email) });
+
+  readonly emailForm: Observable<string> = this.form.valueChanges.pipe(map((data) => data['email']))
+  onFormSubmitted(form: FormGroup): void {
+  }
 }
