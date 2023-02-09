14c79a87c51c89e65d1a80efa8ca651703d1cdae
Usshel <pawelopadczuk@gmail.com>
2023-02-09 16:34:37 +0100

    feat: control-age component created

---
+++ src/app/components/form-control-age/form-control-age.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { FormControlAgeComponent } from './form-control-age.component';
+
+@NgModule({
+  imports: [],
+  declarations: [FormControlAgeComponent],
+  providers: [],
+  exports: [FormControlAgeComponent]
+})
+export class FormControlAgeComponentModule {
+}
