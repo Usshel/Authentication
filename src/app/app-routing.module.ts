6035834d64daf7feb12bfa8ba573067b034f2f58
Usshel <pawelopadczuk@gmail.com>
2023-02-10 11:14:55 +0100

    feat: control-comment done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule } from '@angular/router';
 import { FormControlNameComponent } from './components/form-control-name/form-control-name.component';
 import { FormControlAgeComponent } from './components/form-control-age/form-control-age.component';
 import { FormControlEmailComponent } from './components/form-control-email/form-control-email.component';
+import { ControlCommentComponent } from './components/control-comment/control-comment.component';
 import { FormControlNameComponentModule } from './components/form-control-name/form-control-name.component-module';
 import { FormControlAgeComponentModule } from './components/form-control-age/form-control-age.component-module';
 import { FormControlEmailComponentModule } from './components/form-control-email/form-control-email.component-module';
+import { ControlCommentComponentModule } from './components/control-comment/control-comment.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'control-name', component: FormControlNameComponent }, { path: 'control-age', component: FormControlAgeComponent }, { path: 'control-email', component: FormControlEmailComponent }]), FormControlNameComponentModule, FormControlAgeComponentModule, FormControlEmailComponentModule],
+  imports: [RouterModule.forRoot([{ path: 'control-name', component: FormControlNameComponent }, 
+  { path: 'control-age', component: FormControlAgeComponent }, 
+  { path: 'control-email', component: FormControlEmailComponent },
+   { path: 'control-comment', component: ControlCommentComponent }]), FormControlNameComponentModule, FormControlAgeComponentModule, FormControlEmailComponentModule, ControlCommentComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
